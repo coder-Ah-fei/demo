829bb56b445a0d2c0c54dad25cedb926df5b59f3
Administrator <yiixuan@163.com>
2018-09-06 15:00:38 +0800

    mybatis根据表生成pojo类，dao类，mapper.xml文件

---
+++ table-create-pojo/src/main/java/com/example/tablecreatepojo/TableCreatePojoApplication.java
@@ -0,0 +1,12 @@
+package com.example.tablecreatepojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableCreatePojoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TableCreatePojoApplication.class, args);
+    }
+}
---
+++ table-create-pojo/src/test/java/com/example/tablecreatepojo/TableCreatePojoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.tablecreatepojo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TableCreatePojoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
